e3baad1325995beee0d669344bd5d56866199678
Mariusz <mpastuszka@gmail.com>
2024-02-11 09:27:36 +0100

    [add] - more Person related classes (even records)

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -1,5 +1,6 @@
 package com.sda;
 
+import com.sda.model.ImmutablePerson;
 import com.sda.model.Person;
 
 public class Main {
@@ -8,5 +9,14 @@ public class Main {
 
         Person me = new Person("Mariusz", "Pastuszka", 18);
         System.out.println("Hi, it's me: " + me);
+
+        me.setName("Maniek");
+        System.out.println("With changed name: " + me);
+
+        ImmutablePerson meAgain = new ImmutablePerson("Mariusz", "Pastuszka", 21);
+        System.out.println("Me again: " + meAgain);
+
+        ImmutablePerson fixedMe = new ImmutablePerson(meAgain.getName(), meAgain.getSurname(), 42);
+        System.out.println("Fixed me: " + fixedMe);
     }
 }
---
+++ src/main/java/com/sda/model/PersonRecord.java
@@ -0,0 +1,8 @@
+package com.sda.model;
+
+public record PersonRecord(
+        String name,
+        String surname,
+        int age
+) {
+}
