f4b3c28e4107d6601f4046b5ec982628d0542e41
Mariusz <mpastuszka@gmail.com>
2024-02-11 09:44:18 +0100

    [change] - example of using PersonRecord

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -2,6 +2,7 @@ package com.sda;
 
 import com.sda.model.ImmutablePerson;
 import com.sda.model.Person;
+import com.sda.model.PersonRecord;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,5 +19,9 @@ public class Main {
 
         ImmutablePerson fixedMe = new ImmutablePerson(meAgain.getName(), meAgain.getSurname(), 42);
         System.out.println("Fixed me: " + fixedMe);
+
+        System.out.println("Let's play with records");
+        PersonRecord record = new PersonRecord("Mariusz", "Pastuszka", 42);
+        System.out.println("Record person: " + record);
     }
 }
