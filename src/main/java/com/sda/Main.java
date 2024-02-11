a0598ccc5528f44208c3dd70e90a869173eaa344
Mariusz <mpastuszka@gmail.com>
2024-02-11 09:07:44 +0100

    [add] - Person class created

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -1,7 +1,12 @@
 package com.sda;
 
+import com.sda.model.Person;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Person me = new Person("Mariusz", "Pastuszka", 18);
+        System.out.println("Hi, it's me: " + me);
     }
 }
